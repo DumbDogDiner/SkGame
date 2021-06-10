4c7142279ef6c543ce23724313299024adbf8503
SkyezerFox <actuallyori@gmail.com>
2021-06-10 10:18:29 +0100

    add pre-commit formatting

--- minigame/src/main/kotlin/com/dumbdogdiner/skgame/minigame/ecs/EntityInternal.kt
+++ minigame/src/main/kotlin/com/dumbdogdiner/skgame/minigame/ecs/EntityInternal.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020-2021 DumbDogDiner <dumbdogdiner.com>. All rights reserved.
+ * Licensed under the MIT license, see LICENSE for more information...
+ */
 package com.dumbdogdiner.skgame.minigame.ecs
 
 import com.dumbdogdiner.skgame.minigame.ecs.annotation.Entity
@@ -8,12 +12,15 @@ import kotlin.reflect.full.hasAnnotation
  * Represents an internal entity.
  */
 internal class EntityInternal(child: Any) {
-	init {
-		// ensure annotation
-		if (!child::class.hasAnnotation<Entity>()) {
-			throw InvalidAnnotationException(child, Entity::class)
-		}
-	}
+    init {
+        // ensure annotation
+        if (!child::class.hasAnnotation<Entity>()) {
+            throw InvalidAnnotationException(child, Entity::class)
+        }
+    }
 
-	val components = listOf<ComponentInternal>()
+    /**
+	 * A list of components attached to this entity.
+	 */
+    val components = listOf<ComponentInternal>()
 }
